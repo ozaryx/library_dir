a672fdacad93c68acc021e928c9c317130a88be3
Mankov Konstantin <k.mankov@cft.ru>
2019-01-29 09:46:39 +0700

    fix output

--- get_books_info.py
+++ get_books_info.py
@@ -1,18 +1,19 @@
 import library_dir as ld
 
 library = 'library'
+
 content = ld.read_library_description(library)
-print(content)
+print(content, '\n')
 
 book_list = ld.get_book_files(library)
-print(book_list)
+print(book_list, '\n')
 
 book_file = 'O_narodnom_vospitanii.txt'
 book_info = ld.read_book_info(library, book_file)
-print(book_info)
+print(book_info, '\n')
 
 authors = ld.get_authors(library)
-print(authors)
+print(authors, '\n')
 
 books_info = ld.get_all_books_info(library)
-print(books_info)
+print(books_info, '\n')
